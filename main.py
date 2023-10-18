7efdeaf70f8561e53e5ae9fa513cdcc94baae158
Andrey Evgl <evgl2004@yandex.ru>
2023-10-18 18:47:18 +0500

    Изменение №04: Добавлено консольное меню для взаимодействия с данными полученными по API и хранящимся в Базе Данных.

--- main.py
+++ main.py
@@ -5,19 +5,20 @@ from src.db import DBManager
 
 def main():
 
+    # Список с кодами работодателей с сайта HH.ru
     list_favorite_employer = [
         '1122462', '80', '15478', '3127', '193400',
         '4023', '22259', '87279', '544224', '900947'
     ]
 
-    # считываем параметры подключения из файла с настройками *.ini
+    # Считываем параметры подключения из файла с настройками *.ini
     params = read_config()
 
-    # создаём базу данных
+    # Создаём базу данных
     create_database('vacancies', params)
     print("БД 'vacancies' успешно создана")
 
-    # создаём таблицы для базы данных
+    # Создаём таблицы для базы данных
     create_tables('vacancies', params)
     print("Таблицы успешно созданы")
 
@@ -35,22 +36,58 @@ def main():
 
     print("Данные успешно записаны в БД")
 
+    # Инициализация экземпляра класса для работы с базой данных и имеющий несколько методов с различными выборками.
     db_vacancies = DBManager('vacancies', params)
 
-    for selection_detailed_records in db_vacancies.get_companies_and_vacancies_count():
-        print(selection_detailed_records)
+    input_data = ""
 
-    for selection_detailed_records in db_vacancies.get_all_vacancies():
-        print(selection_detailed_records)
+    # Основное меню выбора команд взаимодействия с полученными данными.
+    # Цикл до тех пор, пока не будет введено ключевое слово выхода.
+    while input_data.lower() != 'exit':
 
-    for selection_detailed_records in db_vacancies.get_avg_salary():
-        print(selection_detailed_records)
+        input_data = input(
+            "Выберите пункт:\n"
+            "1 - Вывести список всех компаний и количество вакансий у каждой компании.\n"
+            "2 - Вывести список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на"
+            " вакансию.\n"
+            "3 - Вывести среднюю зарплату по всем вакансиям.\n"
+            "4 - Вывести список всех вакансий, у которых зарплата выше средней среди всех вакансий.\n"
+            "5 - Вывести список всех вакансии, в названии которых содержится ключевое слово.\n"
+            "exit - завершить работу.\n")
 
-    for selection_detailed_records in db_vacancies.get_vacancies_with_higher_salary():
-        print(selection_detailed_records)
+        if input_data == '1':
+            # Обходим и выводим результаты выборки списка всех компаний и количество вакансий у каждой компании.
+            for selection_detailed_records in db_vacancies.get_companies_and_vacancies_count():
+                print(selection_detailed_records)
 
-    for selection_detailed_records in db_vacancies.get_vacancies_with_keyword("Продавец"):
-        print(selection_detailed_records)
+        elif input_data == '2':
+            # Обходим и выводим результаты выборки список всех вакансий с указанием названия компании, названия вакансии
+            # и зарплаты и ссылки на вакансию.
+            for selection_detailed_records in db_vacancies.get_all_vacancies():
+                print(selection_detailed_records)
+
+        elif input_data == '3':
+            # Обходим и выводим результаты выборки средней зарплаты по всем вакансиям.
+            for selection_detailed_records in db_vacancies.get_avg_salary():
+                print(selection_detailed_records)
+
+        elif input_data == '4':
+            # Обходим и выводим результаты выборки список всех вакансий, у которых зарплата выше средней среди
+            # всех вакансий.
+            for selection_detailed_records in db_vacancies.get_vacancies_with_higher_salary():
+                print(selection_detailed_records)
+
+        elif input_data == '5':
+
+            input_key_word = input(
+                "Укажите ключевое слово для поиска в названиях вакансий:\n")
+
+            # Обходим и выводим результаты выборки всех вакансии, в названии которых содержится ключевое слово.
+            for selection_detailed_records in db_vacancies.get_vacancies_with_keyword(input_key_word):
+                print(selection_detailed_records)
+
+        elif input_data != 'exit':
+            print("Введите число от 1 до 5.")
 
 
 if __name__ == "__main__":
